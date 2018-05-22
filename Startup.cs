4ac59602a374babddaa18e267d36f5daa0d61d29
Juan José Rendon <juan.rendon@yuxiglobal.com>
2018-05-21 22:57:51 -0500

    feat: allow CORS policies

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +37,9 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            // Tell the app to allow all CORS policies.
+            // Should research more, don't think this is secure enough.
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
